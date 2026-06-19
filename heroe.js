40f02d0695a521e3e33d99cf0c2d1698be6f8305
Jefferson <erneypianda@gmail.com>
2026-06-19 18:49:27 +0200

    Enhance hero display with additional stats
    Added base health, mana, and armor display for hero.

--- heroe.js
+++ heroe.js
@@ -1,5 +1,12 @@
 //lee el heroe guardado//
+
 let heroeGuardado = localStorage.getItem("heroeSeleccionado")
+if(!heroeGuardado){
+   window.location.href="index.html";
+}
+console.log("heroe.js cargado")
+console.log(heroeGuardado)
+
  let heroe = JSON.parse(heroeGuardado)
  //muestra los datos//
  let nombreLimpio = heroe.name.replace("npc_dota_hero_","")
@@ -12,11 +19,17 @@ document.getElementById("nombreHeroe").textContent=heroe.localized_name;
 document.getElementById("atributo").textContent=heroe.primary_attr;
 document.getElementById("tipoAtaque").textContent=heroe.attack_type;
 document.getElementById("roles").textContent=heroe.roles.join(", ");
+document.getElementById("vidaBase").textContent=heroe.base_health;
+document.getElementById("manaBase").textContent=heroe.base_mana;
+document.getElementById("armaduraBase").textContent=heroe.base_armor;
+
 //botoonvolver//
+console.log("heroe.js cargado");
+console.log(localStorage.getItem("heroeSeleccionado"));
+
 document.getElementById("btnVolver")
 .addEventListener("click",()=>{
-   window.location.href="index.html";
-   console.log("heroe.js cargado");
-console.log(localStorage.getItem("heroeSeleccionado"));
+window.location.href="index.html";
+
 
 });
