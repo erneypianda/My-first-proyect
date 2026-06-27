6f85db539b58f658b7eddaec88e0603523d3771e
Jefferson <erneypianda@gmail.com>
2026-06-27 02:08:14 +0200

    Update perfil.js

--- perfil.js
+++ perfil.js
@@ -1,114 +1,89 @@
+let filtroAtributoActual = "all";
+let todosLosHeroes=[];
 
-const botonBuscarJugador = document.querySelector("#btnbuscarJugador");
-const inputSteamId = document.querySelector("#inputSteamId");
-const perfilJugador = document.querySelector("#perfilJugador");
+const contenedorBuscar = document.querySelector(".contenedor-buscar");
+const botonBuscar = document.querySelector(".btn-buscar");
+const buscadorInput = document.querySelector(".buscador-input");
+const contenedorCards = document.querySelector(".contenedor-cards");
 
-botonBuscarJugador.addEventListener("click", () => {
-    const id = inputSteamId.value.trim();
+const linkApi = "https://api.opendota.com/api/heroStats";
 
-    if(id === "") {
-        alert("Debe introducir un ID");
+// ← FUNCIÓN que muestra héroes (nueva)
+function mostrarHeroes(listaHeroes) {
+    contenedorCards.innerHTML = "";
+
+    listaHeroes.forEach(heroe => {
+        let nombreLimpio = heroe.name.replace("npc_dota_hero_", "");
+        let imagen = `https://cdn.cloudflare.steamstatic.com/apps/dota2/images/dota_react/heroes/${nombreLimpio}.png`;
+
+        let div = document.createElement("div");
+        div.classList.add("card-heroe");
+        div.innerHTML = `
+            <img src="${imagen}" alt="${heroe.localized_name}">
+            <p>${heroe.localized_name}</p>
+        `;
+
+        div.addEventListener("click", () => {
+            localStorage.setItem("heroeSeleccionado", JSON.stringify(heroe));
+            window.location.href = "heroe.html";
+        });
+
+        contenedorCards.appendChild(div);
+    });
+}
+function filtrarYMostrar (){
+    const textoBuscado = buscadorInput.value.trim().toLowerCase();
+    //REUBICADO FILTRO ANTIGUO//
+
+    const heroesFiltrados = todosLosHeroes.filter(heroe=>{
+        const coincideNombre = heroe.localized_name.toLowerCase().includes(textoBuscado);
+
+        const coincideAtributo = (filtroAtributoActual==="all")||(heroe.primary_attr === filtroAtributoActual)
+
+        return coincideNombre &&  coincideAtributo ;
+    });
+
+ if(heroesFiltrados.length === 0) {
+    alert("Héroe no encontrado");
+    return;
+}
+mostrarHeroes(heroesFiltrados);
+
+}
+// ← CARGA TODOS AL INICIO (nuevo)
+fetch(linkApi)
+.then(response => response.json())
+.then(heroes => {
+    mostrarHeroes(heroes);
+    todosLosHeroes = heroes;
+});
+
+// ← BUSCADOR FILTRA
+botonBuscar.addEventListener("click", () => {
+    const textoBuscado = buscadorInput.value.trim().toLowerCase();
+
+    if(textoBuscado === "") {
+        alert("Debe introducir el nombre de un heroe");
         return;
     }
-
-    // DATOS DEL JUGADOR
-    fetch(`https://api.opendota.com/api/players/${id}`)
-    .then(response => response.json())
-    .then(jugador => {
-      
-   // DATOS DE WIN Y LOSE
-    fetch (`https://api.opendota.com/api/players/${id}/wl`)
-    .then(response => response.json())
-    .then(winLose =>{
-      
-      let totalPartidas = winLose.win + winLose.lose;
-      let porcentajeVictorias = ((winLose.win/totalPartidas)*100).toFixed(2);
-
-        perfilJugador.innerHTML = `
-       <div class="perfil-card">
-       
-       <img src="${jugador.profile.avatarfull}" alt="avatar" class="avatar-jugador">
-       <h2>${ jugador.profile.personame || "Jugador Anónimo"}</h2>
-
-       <div class=" stats.jugador">
-       <p> Rank Tier: ${jugador.rank_tier}</p>
-       <p> LeaderBoard: ${jugador.leaderboard_rank}</p>
-       <p> Pais : ${jugador.profile.loccountrycode}</p>
-       </div>
-       <divclass="winlose-jugador">
-            <p>✅ Victorias: ${winLose.win}</p>
-            <p>❌ Derrotas: ${winLose.lose}</p>
-            <p>📈 % Victorias: ${porcentajeVictorias}%</p>>
-       </div>
-       </div>
-      
-        `;
-        //fetch de los heroes top5
-
-      fetch("https://api.opendota.com/api/heroStats")
-      .then(response=> response.json())
-        .then(todosHeroes =>{
-        
-        fetch(`https://api.opendota.com/api/players/${id}/heroes`)
-        .then(response=> response.json())
-        .then(topHeroesJugador =>{
-
-            const top5 = topHeroesJugador.slice(0,5);
-             let htmlHeroes = `<h3>🦸 Top 5 Héroes</h3><div class="top-heroes-container">`;
-
-
-            top5.forEach(heroeJugador => {
-    const heroeInfo = todosHeroes.find(h => h.id === heroeJugador.hero_id);
-    let nombreLimpio = heroeInfo.name.replace("npc_dota_hero_", "");
-    let imagenHeroe = `https://cdn.cloudflare.steamstatic.com/apps/dota2/images/dota_react/heroes/${nombreLimpio}.png`;
-
-    htmlHeroes += `
-        <div class="mini-card-heroe">
-            <img src="${imagenHeroe}" alt="${heroeInfo.localized_name}">
-            <p>${heroeInfo.localized_name}</p>
-            <small>${heroeJugador.games} partidas | ${heroeJugador.win} victorias</small>
-        </div>
-    `;
-
-                   });
-
-                 htmlHeroes += `</div>`;
-                 perfilJugador.innerHTML += htmlHeroes;
-                 fetch(`https://api.opendota.com/api/players/${id}/recentMatches`)
-                    .then(response => response.json())
-                    .then(partidas => {
-
-                        const ultimas10 = partidas.slice(0, 10);
-                        let htmlPartidas = `<h3>📜 Últimas 10 Partidas</h3><div class="partidas-container">`;
-
-                        ultimas10.forEach(partida => {
-                            const heroeInfoPartida = todosHeroes.find(h => h.id === partida.hero_id);
-
-                            let esRadiant = partida.player_slot < 128;
-                            let ganaste = (esRadiant && partida.radiant_win) || (!esRadiant && !partida.radiant_win);
-
-                            let minutos = Math.floor(partida.duration / 60);
-                            let segundos = partida.duration % 60;
-
-                            htmlPartidas += `
-                                <div class="card-partida ${ganaste ? 'victoria' : 'derrota'}">
-                                    <p class="resultado">${ganaste ? '✅ Victoria' : '❌ Derrota'}</p>
-                                    <p>${heroeInfoPartida.localized_name}</p>
-                                    <p>⏱️ ${minutos}:${segundos.toString().padStart(2, '0')}</p>
-                                    <p>⚔️ ${partida.kills}/${partida.deaths}/${partida.assists}</p>
-                                </div>
-                            `;
-                        });
-
-                        htmlPartidas += `</div>`;
-                        perfilJugador.innerHTML += htmlPartidas;
-                    });
-
-                });
-
-            });
-        });    
-    });
+filtrarYMostrar();
 });
+
+document.querySelectorAll(".btn-filtro").forEach(boton=>{
+    boton.addEventListener("click",(e)=>{
+        document.querySelectorAll(".btn-filtro").forEach(b=>
+            b.classList.remove("activo")
+        );
+        e.target.classList.add("activo");
+        
+        filtroAtributoActual = e.target.getAttribute("data-atri");
+
+        filtrarYMostrar();
+    });
+
+});
+
+
+
 
 
